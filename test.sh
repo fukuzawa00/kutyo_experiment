b4b526b76eb851bd7a4ac618d869ae63111aa98c
fukuzawa <T119120@ed.sus.ac.jp>
2023-01-12 22:24:24 +0900

    スケジューラー改良

--- test.sh
+++ test.sh
@@ -1,36 +1,129 @@
 #!/bin/bash
 
-#関数ファイル読み込み
+scheduler() {
+        R=`mosquitto_sub -h localhost -t snk/0 -C 1|sed -e 's/,/\n/g'|grep '"r"'|awk '{print $2}'` #現在の温度
+	R1=$1 #設定温度
+        R2=$2 #基準温度
+        R3=$3 #終了温度
+        S=$4 #何度ずつ上げ下げ
+        M=$5 #運転時間
 
-#現在の温度(R3=R)
-R1=20 #設定
-R2=30 #基準温度
-R3=15 #終了温度
-S=5 #何度ずつ上げ下げ
-M=15 #運転時間
+	#現在温度が基準温度より小さい(R1<R2)
+	if [ "$R1" -lt "$R2" ]; then
+		#スケジュール運転
+        	echo "スケジュール運転開始"
+        	mosquitto_pub -h localhost -t snk/1 -m "{\"r\": $R1}"
+        	echo "r:$R1 ℃ 運転時間:$M m"
+        	sleep "$M"s
 
-while [ $R1 -lt $R2 ]
-do
-	#(3)
-        R1=$((R1+S))	#S℃ずつ上げる
-        mosquitto_pub -h localhost -t snk/1 -m "{\"r\": $R1}"
-	sleep $M s	#M分経過
-        echo "rを$S℃ずつ上昇"
-        echo "r:$R1"
+        	while [ "$R1" -lt "$R2" ]	#R1<R2
+        	do
+                	#(3)
+                	R1=$((R1+S))    #S℃ずつ上げる
+                	echo "rを$S℃ずつ上昇"
+                	mosquitto_pub -h localhost -t snk/1 -m "{\"r\": $R1}"
+                	echo "r:$R1 ℃ 運転時間:$M m"
+                	sleep "$M"s     #M分経過
 
-	if [ $R1 -eq $R2 ]; then
-		while [ $R1 > R$3 ]
-		do
-			#(4)
-			R1=$((R1-S)) #S℃ずつ下げる
-			mosquitto_pub -h localhost -t snk/1 -m "{\"r\": $R1}"
-			echo "rを$S℃ずつ下降"
-			echo "r:$R1"
+                	if [ $R1 -eq $R2 ]; then
+				#終了温度(元運転していた時のr)が現在温度より小さい(R1>R3)とき
+                        	while [ "$R1" -gt "$R3" ]
+                        	do
+                                	#(4)
+                                	R1=$((R1-S)) #S℃ずつ下げる
+                                	echo "rを$S℃ずつ下降"
+                                	mosquitto_pub -h localhost -t snk/1 -m "{\"r\": $R1}"
+                                	echo "r:$R1 ℃ 運転時間:$M m"
+                                	sleep "$M"s     #M分経過
 
-			#(5)
-			if [ $R1 -eq $R3 ]; then
-				exit 0
+                                	#(5)
+                                	if [ $R1 -eq $R3 ]; then
+                                        	echo "運転終了"
+                                        	exit 0
+                                	fi
+                        	done
+
+				#終了温度(元運転していた時のr)が現在温度より大きい(R1<R3)とき
+                        	while [ "$R1" -lt "$R3" ]
+                        	do
+                                	#(4)
+                                	R1=$((R1+S)) #S℃ずつ上げる
+                                	echo "rを$S℃ずつ上昇"
+                                	mosquitto_pub -h localhost -t snk/1 -m "{\"r\": $R1}"
+                                	echo "r:$R1 ℃ 運転時間:$M m"
+                                	sleep "$M"s     #M分経過
+
+                                	#(5)
+                                	if [ $R1 -eq $R3 ]; then
+                                        	echo "運転終了"
+                                        	exit 0
+                                	fi
+                        	done
+                	fi
+        	done
+
+	#現在温度が基準温度より大きい(R1>R2)
+	elif [ "$R1" -gt "$R2" ]; then
+		#スケジュール運転
+                echo "スケジュール運転開始"
+                mosquitto_pub -h localhost -t snk/1 -m "{\"r\": $R1}"
+                echo "r:$R1 ℃ 運転時間:$M m"
+                sleep "$M"s
+
+                while [ "$R1" -gt "$R2" ]       #R1>R3
+                do
+                	#(3)
+                        R1=$((R1-S))    #S℃ずつ上げる
+                        echo "rを$S℃ずつ下降"
+                        mosquitto_pub -h localhost -t snk/1 -m "{\"r\": $R1}"
+                        echo "r:$R1 ℃ 運転時間:$M m"
+                        sleep "$M"s     #M分経過
+
+                        if [ $R1 -eq $R2 ]; then
+                        	#終了温度(元運転していた時のr)が現在温度より小さい(R1>R3)とき
+                                while [ "$R1" -gt "$R3" ]       #R1>R3
+                                do
+                                	#(4)
+                                        R1=$((R1-S)) #S℃ずつ下げる
+                                        echo "rを$S℃ずつ下降"
+                                        mosquitto_pub -h localhost -t snk/1 -m "{\"r\": $R1}"
+                                        echo "r:$R1 ℃ 運転時間:$M m"
+                                        sleep "$M"s     #M分経過
+
+                                        #(5)
+                                        if [ $R1 -eq $R3 ]; then
+                                        	echo "運転終了"
+                                                exit 0
+                                        fi
+                       		done
+
+                                #終了温度(元運転していた時のr)が現在温度より大きい(R1<R3)とき
+                                while [ "$R1" -lt "$R3" ]
+                                do
+                                        #(4)
+                                        R1=$((R1+S)) #S℃ずつ上げる
+                                        echo "rを$S℃ずつ上昇"
+                                        mosquitto_pub -h localhost -t snk/1 -m "{\"r\": $R1}"
+                                        echo "r:$R1 ℃ 運転時間:$M m"
+                                        sleep "$M"s     #M分経過
+
+                                        #(5)
+                                        if [ $R1 -eq $R3 ]; then
+                                                echo "運転終了"
+                                                exit 0
+                                        fi
+                                done
 			fi
 		done
+	else
+		echo "NO"
 	fi
-done
+}
+
+R1=15 #最初の設定温度
+R2=10 #基準温度
+R3=15 #終了温度
+S=1 #何度ずつ上げ下げ
+M=25 #運転時間
+
+scheduler $R1 $R2 $R3 $S $M
